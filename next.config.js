0283839a1a5379c33ece687aebfad14379e87eec
xossiane <josiane.lima@valtech.com>
2023-02-23 12:04:39 -0300

    Add: Readme and import sass globally

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  sassOptions: {
+    additionalData: `@import "src/styles/main.scss";`,
+  }
 }
 
 module.exports = nextConfig
