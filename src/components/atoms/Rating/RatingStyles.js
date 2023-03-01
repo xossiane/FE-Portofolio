721cdd2e56df014e10e592e6202e55648593fee0
xossiane <anejosi.lima@gmail.com>
2023-03-01 17:31:47 -0300

    Fix: Star rating with precision (0.5)

---
+++ src/components/atoms/Rating/RatingStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+  
+export const Container = styled.div`
+   display: flex;
+   justify-content: left;
+   margin: 3.6rem 0;
+   align-items: center;
+   font-size: 4rem;
+`
+export const Radio = styled.input`
+   display: none;
+`
+export const Rating = styled.div`
+   cursor: pointer;
+`
--- src/components/molecules/Testimonials/index.jsx
+++ src/components/molecules/Testimonials/index.jsx
@@ -2,12 +2,14 @@ import Text from '@/components/atoms/Text'
 import styles from './styles.module.scss'
 import React from 'react'
 import Avatar from '../Avatar'
+import Rating from '@/components/atoms/Rating/Rating2'
 
 function Testimonial({}) {
   return (
     <article className={styles.Testimonial}>
 <section className={styles.Testimonial__stars}>
-    <p>stars stars stars</p>
+   <Rating precision={0.5}/>
+  
 </section>
 <section  className={styles.Testimonial__text}>
 <Text color="darkBrown" size="medium">A digital agency is a business you hire to outsource your digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you. </Text>
