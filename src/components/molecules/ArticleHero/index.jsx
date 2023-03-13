51af1d9df576d17736e0998a4cd0a40a5a9e659b
xossiane <josiane.lima@valtech.com>
2023-03-13 16:15:07 -0300

    Add: create Article Hero function

---
+++ src/components/molecules/ArticleHero/index.jsx
@@ -0,0 +1,56 @@
+import Heading from "@/components/atoms/Heading";
+import Text from "@/components/atoms/Text";
+import React from "react";
+import styles from "./styles.module.scss";
+
+function ArticleHero({ title, subtitle, desc, img }) {
+  const category = [
+    { name: "Art Direction" },
+    { name: "User Interface" },
+    { name: "Branding Strategy" },
+    { name: "Print Design" },
+    { name: "3D Render" },
+  ];
+  return (
+    <div className={styles.ArticleHero}>
+      <Heading level="2" pageTitle={false} color="darkBrown" align="left">
+        {title}
+      </Heading>
+      <Text className={styles.ArticleHero__title} color="darkBrown">
+        {subtitle}
+      </Text>
+      <Text className={styles.ArticleHero__title} color="lightBrown">
+        {desc}
+      </Text>
+      <div className={styles.ArticleHero__content}>
+        <span className={styles.ArticleHero__category}>
+          <Heading level="5" color="darkOrange" align="left">
+            CATEGORY
+          </Heading>
+          {category.map((item) => (
+            <Text key={item.name} size="small" color="darkBrown">
+              {item.name}
+            </Text>
+          ))}
+        </span>
+        <div className={styles.ArticleHero__client}>
+          <Heading level="5" color="darkOrange" align="left">
+            CLIENT
+          </Heading>
+          <img src={img} alt="img" />
+        </div>
+
+        <span className={styles.ArticleHero__date}>
+          <Heading level="5" color="darkOrange" align="left">
+            DATE
+          </Heading>
+          <Text color="darkBrown" size="small">
+            25. FEB. 23
+          </Text>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default ArticleHero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Testimonial from "@/components/molecules/Testimonials";
 import TestimonialsWrapper from "@/components/organisms/TestimonialsWrapper";
 import Card from "@/components/molecules/Card";
 import CardsWrapper from "@/components/organisms/CardsWrapper";
+import ArticleHero from "@/components/molecules/ArticleHero";
 
 
 export default function Home() {
@@ -20,7 +21,7 @@ Design Agency"
         description="Ability to put themselves in the merchant's shoes. It is meant to partner on the long run, and work as an extension of the merchant's team."
       ></Hero>
        <CCWrapper></CCWrapper>
-
+<ArticleHero title="Work Media Project" subtitle="Agency provides a full service range including technical skills, design, business understanding." desc="Outsource your digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you." img="/client.png"></ArticleHero>
      <TestimonialsWrapper /> 
     <CardsWrapper></CardsWrapper>
     <Hero
@@ -32,6 +33,7 @@ Design Agency"
         description="Ability to put themselves in the merchant's shoes. It is meant to partner on the long run, and work as an extension of the merchant's team."
         variation="variation"
       ></Hero>
+
     </>
   );
 }
